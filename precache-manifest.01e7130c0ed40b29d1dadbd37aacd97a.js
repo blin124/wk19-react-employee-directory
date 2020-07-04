1a3b4b1f07f444537f68547ccf64bcbb874665f8
Bao <lbx01072004@gmail.com>
2020-07-04 14:32:28 +1000

    Updates

---
+++ precache-manifest.01e7130c0ed40b29d1dadbd37aacd97a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49eef177e374422e9b17845e7a993aff",
+    "url": "/wk19-react-employee-directory/index.html"
+  },
+  {
+    "revision": "c6b1df8cf29a711ffba6",
+    "url": "/wk19-react-employee-directory/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4c9881e5e5aa9709c9a8",
+    "url": "/wk19-react-employee-directory/static/js/2.b3e418df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wk19-react-employee-directory/static/js/2.b3e418df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b1df8cf29a711ffba6",
+    "url": "/wk19-react-employee-directory/static/js/main.7cb536bc.chunk.js"
+  },
+  {
+    "revision": "786e529a605d351fc631",
+    "url": "/wk19-react-employee-directory/static/js/runtime-main.5d33c723.js"
+  }
+]);
